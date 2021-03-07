62492e9c8d1a766809dbe2bcec4a09bc8c2fa970
YOGI JUNIOR <yogi.junior4091@gmail.com>
2021-03-08 00:57:14 +0800

    Fix Prediction Math

---
+++ main.py
@@ -0,0 +1,109 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+import pandas_datareader as web
+import datetime as dt
+import pprint
+
+
+from sklearn.preprocessing import MinMaxScaler
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Dropout, LSTM
+
+#Memuat Data Perusahaan
+
+print(f'\n PHSD V1.0 tools Created by: @_yogijr \n')
+
+
+company = input("Masukan Simbol/Kode Perusahaan Yang ingin di prediksi :")
+
+start = dt.datetime(2012,1,1)
+end = dt.datetime(2021,1,1)
+
+data = web.DataReader(company, 'yahoo', start, end)
+
+
+#Menyiapkan Data
+scaler = MinMaxScaler(feature_range=(0,1))
+scaled_data = scaler.fit_transform(data['Close'].values.reshape(-1,1))
+
+
+prediction_days = 60
+
+x_train = []
+y_train = []
+
+for x in range(prediction_days, len(scaled_data)):
+    x_train.append(scaled_data[x-prediction_days:x, 0])
+    y_train.append(scaled_data[x, 0])
+
+x_train, y_train = np.array(x_train), np.array(y_train)
+x_train = np.reshape(x_train, (x_train.shape[0], x_train.shape[1], 1))
+
+#Membangun Modul
+model = Sequential()
+
+model.add(LSTM(units=50, return_sequences=True, input_shape=(x_train.shape[1], 1)))
+model.add(Dropout(0.2))
+model.add(LSTM(units=50, return_sequences=True))
+model.add(Dropout(0.2))
+model.add(LSTM(units=50))
+model.add(Dropout(0.2))
+model.add(Dense(units=1))
+
+model.compile(optimizer='adam', loss='mean_squared_error')
+model.fit(x_train, y_train, epochs=25, batch_size=32)
+
+
+'''Test The All Model Accuracy on existing data '''
+
+#Memuat Data Test
+test_start = dt.datetime(2020,1,1)
+test_end = dt.datetime.now()
+
+test_data = web.DataReader(company, 'yahoo', test_start, test_end)
+actual_prices = test_data['Close'].values
+
+
+total_dataset = pd.concat((data['Close'], test_data['Close']), axis=0)
+
+
+model_inputs = total_dataset[len(total_dataset) - len(test_data) - prediction_days:].values
+model_inputs = model_inputs.reshape(-1, 1)
+model_inputs = scaler.transform(model_inputs)
+
+
+#Membuat Prediksi Berdasarkan Data test
+
+x_test = []
+
+for x in range(prediction_days, len(model_inputs)):
+    x_test.append(model_inputs[x-prediction_days:x, 0])
+
+x_test = np.array(x_test)
+x_test = np.reshape(x_test, (x_test.shape[0], x_test.shape[1], 1))
+
+predicted_prices = model.predict(x_test)
+predicted_prices = scaler.inverse_transform(predicted_prices)
+
+#Plot Untuk Test Prediksi
+plt.plot(actual_prices, color="red", label=f"Harga Nyata {company} ")
+plt.plot(predicted_prices, color='blue', label=f"Prediksi Harga {company} ")
+plt.title(f"Harga Saham {company} ")
+plt.xlabel('Waktu')
+plt.ylabel(f'Harga Saham {company} ')
+plt.legend()
+plt.show()
+
+
+
+#Prediksi Yang Akan Datang
+
+real_data = [model_inputs[len(model_inputs) + 1 - prediction_days:len(model_inputs+1), 0]]
+real_data = np.array(real_data)
+real_data = np.reshape(real_data, (real_data.shape[0], real_data.shape[1], 1))
+
+prediction = model.predict(real_data)
+prediction = scaler.inverse_transform(prediction)
+
+print(f"\nPrediksi Untuk Besok: {prediction}")
